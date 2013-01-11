9cc81c64250670107e90ab85b321315e97b95abc
basiclines <igonzaleznicolas@gmail.com>
2013-01-11 09:59:13 +0100

    [make] Support owd branding

--- build/webapp-zip.js
+++ build/webapp-zip.js
@@ -22,7 +22,7 @@ const PR_EXCL = 0x80;
  */
 function addToZip(zip, pathInZip, file) {
   if (isSubjectToBranding(file.path)) {
-    file.append((OFFICIAL == 1) ? 'official' : 'unofficial');
+    file.append((OFFICIAL == 1) ? 'owd' : 'owd');
   }
 
   if (!file.exists())
@@ -282,7 +282,7 @@ Gaia.webapps.forEach(function(webapp) {
     path.split('/').forEach(function(segment) {
       file.append(segment);
       if (isSubjectToBranding(file.path)) {
-        file.append((OFFICIAL == 1) ? 'official' : 'unofficial');
+        file.append((OFFICIAL == 1) ? 'owd' : 'owd');
       }
     });
     if (!file.exists()) {
